b89ed9f66bd8c8432076d2c04bdd169597e9dff3
Jeroen Cornelissen <jeroen@smartbit.be>
2017-06-06 13:39:20 +0200

    initial commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import 'src/mycomponent';
+
+import QUnit from "steal-qunit";
+import F from "funcunit";
+
+QUnit.test('ViewModel', function (assert) {
+	assert.equal(1, 1, 'test assertion');
+});
